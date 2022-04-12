11360d9950f215549c6e07c2fa92acd555e97e41
christian <mabundachristian@gmail.com>
2022-04-12 07:37:23 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,8 +11,8 @@ void print_alphabet_x10(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for(c = 'a'; c <= 'z'; c++)
+		for (c = 'a'; c <= 'z'; c++)
 			_putchar(c);
-		_putchar('\n')
+		_putchar('\n');
 	}
 }
