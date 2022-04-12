f53861f92a4305711fe8a459a22e77dd7c953dca
christian <mabundachristian@gmail.com>
2022-04-12 07:22:32 -0700

    functions nested loops

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98
+ * @n: takes in an integer
+ *
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n; i > 97; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	else
+	{
+		for (i = i; i < 99; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf((", ");
+			}
+		}
+	}
+	printf("\n");
+}
